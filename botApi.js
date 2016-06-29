41f76d6228c39e8d4bc3e77b519104e92727bcf8
Daniel Stucki <daniel@stucki.me>
2016-06-29 10:57:47 +0200

    extract botApi

---
+++ botApi.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+const BotApi = {
+    inviteBotToPlay(bot, wsUrl, callBack) {
+        const post_data = {
+            wsUrl
+        };
+
+        const post_options = {
+            host: bot.host,
+            port: bot.port,
+            path: bot.path,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        // Set up the request
+        const post_req = http.request(post_options, function (res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+                callBack(200);
+            });
+
+        });
+
+        post_req.on('error', (chunk) => {
+            console.log('ErrorResponse: ' + chunk);
+            callBack(500);
+        });
+
+        post_req.write(JSON.stringify(post_data));
+        post_req.end();
+    }
+};
+module.exports = BotApi;
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 const BotStore = require('./botStore');
-const http = require('http');
-
+const BotApi = require('./botApi');
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,7 +16,6 @@ router.post('/', (req, res) => {
     res.sendStatus(200);
 });
 
-
 router.post('/addBot', (req, res) => {
     console.log('received addBot:');
     console.log(req.body);
@@ -26,7 +24,7 @@ router.post('/addBot', (req, res) => {
     if (!botToAdd) {
         console.log('no bot found for' + req.body.botId);
     }
-    registerBotToRegistry(botToAdd, req.body.wsUrl, (status) => res.sendStatus(status));
+    BotApi.inviteBotToPlay(botToAdd, req.body.wsUrl, (status) => res.sendStatus(status));
 });
 
 router.get('/', (req, res) => {
@@ -39,38 +37,3 @@ app.use('/api', router);
 
 app.listen(1338);
 console.log('server listening on port 1338');
-
-function registerBotToRegistry(bot, wsUrl, callBack) {
-    const post_data = {
-        wsUrl
-    };
-
-    const post_options = {
-        host: bot.host,
-        port: bot.port,
-        path: bot.path,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-
-    // Set up the request
-    const post_req = http.request(post_options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-            callBack(200);
-        });
-
-    });
-
-    post_req.on('error', (chunk) => {
-        console.log('ErrorResponse: ' + chunk);
-        callBack(500);
-    });
-
-    post_req.write(JSON.stringify(post_data));
-    post_req.end();
-
-}
